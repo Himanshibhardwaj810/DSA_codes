adaef9928145c000be8904734ec8c99fdda3aafe
Himanshi <bhardwaj.himanshi2468@gmail.com>
2024-12-14 14:49:54 +0530

    leetcode prob

--- linked_list/ques/doubly_kthrotation.java
+++ linked_list/ques/doubly_kthrotation.java
@@ -23,7 +23,7 @@ public class rotatae_doubly_linked_list {
 		
 		Node curr=head;
 		Node prev=null;
-		Node next=prev;
+		Node next=null;
 		int count=0;
 		while(curr!=null && count<k) {
 			next=curr.next;
